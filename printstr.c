62d89a5d511cec1a1cd533ccae0462c5dc3b60c2
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-07-23 16:11:59 +0000

    Task 5

--- printstr.c
+++ printstr.c
@@ -1,22 +1,19 @@
 #include "main.h"
 
 /**
- * printstr - Print string with special handling for non-printable characters.
- * @format: The format string (not used in this function).
- * @pa: The va_list containing the string to print.
- *
- * Return: The number of characters printed.
+ * printstr- print str.
+ * @pa: gtgtg
+ * @format: format str.
+ * Return: number str for print.
  */
 int printstr(char *format, va_list pa)
 {
 	char *string = va_arg(pa, char *);
-	int contador = 0;
+	int contador;
 	(void)format;
 
 	if (string == NULL)
 		string = "(null)";
-
 	contador = _puts(string);
-
 	return (contador);
 }
