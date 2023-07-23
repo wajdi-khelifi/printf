4f868bad0e806257159983d34ed4c3a50b0a0eb0
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-07-23 15:56:41 +0000

    Task 5

--- printstr.c
+++ printstr.c
@@ -1,37 +1,19 @@
 #include "main.h"
 
 /**
- * print_custom_string - Print a custom string with special handling for non-printable characters.
- * @pa: The va_list containing the string to print.
- *
- * Return: The number of characters printed.
+ * printstr- print str.
+ * @pa: gtgtg
+ * @format: format str.
+ * Return: number str for print.
  */
-int print_custom_string(va_list pa)
+int printstr(char *format, va_list pa)
 {
 	char *string = va_arg(pa, char *);
-	int contador = 0;
-	char ch;
+	int contador;
 	(void)format;
 
 	if (string == NULL)
 		string = "(null)";
-
-	while (*string)
-	{
-		ch = *string;
-		if (ch < 32 || ch >= 127)
-		{
-			contador += _putchar('\\');
-			contador += _putchar('x');
-			contador += _putchar("0123456789ABCDEF"[ch / 16]);
-			contador += _putchar("0123456789ABCDEF"[ch % 16]);
-		}
-		else
-		{
-			contador += _putchar(ch);
-		}
-		string++;
-	}
-	
+	contador = _puts(string);
 	return (contador);
 }
