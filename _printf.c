f7ed0a1af9103401f593fa9085bd6ce21ac89938
Iheb Chaoued <ihebchaoued@gmail.com>
2023-07-23 14:47:11 +0000

    test

--- _printf.c
+++ _printf.c
@@ -1,46 +1,46 @@
-#include <stdio.h>
-#include <stdarg.h>
+#include "main.h"
 
-int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
+int _printf(const char *format, ...)
+{
+	va_list args;
 
-    int chars_printed = 0;
+	va_start(args, format);
 
-    while (*format != '\0') {
-        if (*format == '%') {
-            format++;
-            // Handle conversion specifier
-            switch (*format) {
-                case 'c':
-                    // Print a single character
-                    putchar(va_arg(args, int));
-                    chars_printed++;
-                    break;
-                case 's':
-                    // Print a string
-                    chars_printed += printf("%s", va_arg(args, const char *));
-                    break;
-                case '%':
-                    // Print a single '%' character
-                    putchar('%');
-                    chars_printed++;
-                    break;
-                default:
-                    // Invalid conversion specifier, just print the character after '%'
-                    putchar('%');
-                    putchar(*format);
-                    chars_printed += 2;
-                    break;
-            }
-        } else {
-            // Regular character, print it
-            putchar(*format);
-            chars_printed++;
-        }
-        format++;
-    }
+	int chars_printed = 0;
 
-    va_end(args);
-    return chars_printed;
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					putchar(va_arg(args, int));
+					chars_printed++;
+					break;
+				case 's':
+					chars_printed += printf("%s", va_arg(args, const char *));
+					break;
+				case '%':
+					putchar('%');
+					chars_printed++;
+					break;
+				default:
+					putchar('%');
+					putchar(*format);
+					chars_printed += 2;
+					break;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			chars_printed++;
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (chars_printed);
 }
