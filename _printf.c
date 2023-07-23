4299e0b36dcfe70cfdcd68c1509881b26f366900
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-07-23 16:27:26 +0000

    Task 0

--- _printf.c
+++ _printf.c
@@ -3,48 +3,51 @@
 /**
  * _printf - prints formatted data to stdout
  * @format: string that contains the format to print
+ *
+ * Description: The function uses the `driver` function to identify the
+ * Print function for each conversion specifier in the format string.
+ *
  * Return: number of characters written
  */
 int _printf(char *format, ...)
 {
-	int written = 0, (*structype)(char *, va_list);
+	int written = 0;
+	int (*structype)(char *, va_list);
 	char q[3];
 	va_list pa;
 
 	if (format == NULL)
 		return (-1);
+
 	q[2] = '\0';
 	va_start(pa, format);
 	_putchar(-1);
-	while (format[0])
+
+	while (*format)
 	{
-		if (format[0] == '%')
+		if (*format == '%')
 		{
 			structype = driver(format);
 			if (structype)
 			{
 				q[0] = '%';
-				q[1] = format[1];
+				q[1] = *(format + 1);
 				written += structype(q, pa);
-			}
-			else if (format[1] != '\0')
-			{
-				written += _putchar('%');
-				written += _putchar(format[1]);
+				format += 2;
 			}
 			else
 			{
-				written += _putchar('%');
-				break;
+				written += _putchar(*format);
+				format++;
 			}
-			format += 2;
 		}
 		else
 		{
-			written += _putchar(format[0]);
+			written += _putchar(*format);
 			format++;
 		}
 	}
 	_putchar(-2);
+	va_end(pa);
 	return (written);
 }
