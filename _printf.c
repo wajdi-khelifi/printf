9c09dca3873c770d229f0d9093e0e7474e7fc76f
Iheb Chaoued <ihebchaoued@gmail.com>
2023-07-23 14:42:30 +0000

    function that produces output according to a format

--- _printf.c
+++ _printf.c
@@ -1,44 +1,46 @@
-#include "main.h"
-/**
- * _printf - printf function
- * @format: const char pointer
- * Return: b_len
- * this is the start of the file
- */
-int _printf(const char *format, ...)
-{
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+#include <stdio.h>
+#include <stdarg.h>
 
-	register int count = 0;
+int _printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
 
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
-	{
-		if (*p == '%')
-		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
-	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+    int chars_printed = 0;
+
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++;
+            // Handle conversion specifier
+            switch (*format) {
+                case 'c':
+                    // Print a single character
+                    putchar(va_arg(args, int));
+                    chars_printed++;
+                    break;
+                case 's':
+                    // Print a string
+                    chars_printed += printf("%s", va_arg(args, const char *));
+                    break;
+                case '%':
+                    // Print a single '%' character
+                    putchar('%');
+                    chars_printed++;
+                    break;
+                default:
+                    // Invalid conversion specifier, just print the character after '%'
+                    putchar('%');
+                    putchar(*format);
+                    chars_printed += 2;
+                    break;
+            }
+        } else {
+            // Regular character, print it
+            putchar(*format);
+            chars_printed++;
+        }
+        format++;
+    }
+
+    va_end(args);
+    return chars_printed;
 }
